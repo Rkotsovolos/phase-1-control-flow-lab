b07e66304b3cf019bf080823b57336a01497768b
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-11 14:28:20 -0700

    commit tree 98b7fdb0697a9d4f888eb31c007c51bb522d7dd9

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+
+  let string;
+
+  if (num <= 400) {
+    string = 'This one is on me!'
+  }
+  else if (num >= 2001 && num <= 2500) {
+    string = 'I will gladly take your thirty bucks.';
+  }
+  else if (num >= 2500) {
+    string = 'No can do.';
+  }
+  return string;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else 
+    return 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let strings = '';
+
+  if (tip === 'generous') {
+    strings = 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    strings = 'Thank you.';
+  }
+  else if (tip === 'thanks for everything') {
+    strings = 'Bye.';
+  }
+  return strings;
 }
